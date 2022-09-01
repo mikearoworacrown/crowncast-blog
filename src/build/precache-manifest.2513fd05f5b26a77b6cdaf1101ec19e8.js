3636ce65abb0ea8f537464bfe43870fc12a8127e
Michael Aroworade <Ayoaro85@gmail.com>
2022-09-01 14:58:57 +0100

    First commit

---
+++ src/build/precache-manifest.2513fd05f5b26a77b6cdaf1101ec19e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "61ab8110ec3cbd7fcc1b",
+    "url": "/static/css/main.b3318a74.chunk.css"
+  },
+  {
+    "revision": "61ab8110ec3cbd7fcc1b",
+    "url": "/static/js/main.61ab8110.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "694d178330dcb4950932",
+    "url": "/static/js/2.694d1783.chunk.js"
+  },
+  {
+    "revision": "4431f52a260a7d5c73e4",
+    "url": "/static/js/runtime~main.4431f52a.js"
+  },
+  {
+    "revision": "d14ca839d3e23e44c61c0521bd52ffbc",
+    "url": "/index.html"
+  }
+];
